cae41e2be5066566b54d03004cba8b3fdbe38a5e
Prasad Lakshan <77828396+Prasadkpd@users.noreply.github.com>
2022-10-31 21:52:04 +0530

    login done

--- lib/utils/providers.dart
+++ lib/utils/providers.dart
@@ -1,7 +1,10 @@
+import 'package:devhub/view_models/theme/theme_view_model.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 import 'package:devhub/view_models/auth/login_view_model.dart';
 import 'package:provider/single_child_widget.dart';
 
 List<SingleChildWidget> providers = [
-ChangeNotifierProvider(create: (_)=> LoginViewModel())
+  ChangeNotifierProvider(create: (_)=> LoginViewModel()),
+  ChangeNotifierProvider(create: (_)=> ThemeProvider())
 ];
